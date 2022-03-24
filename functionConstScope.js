56d831b2b6661aa630d73545a0768dc41e830d36
FahmiNB <fahmi123ganers@gmail.com>
2022-03-24 09:55:31 +0700

    first commit

---
+++ functionConstScope.js
@@ -0,0 +1,13 @@
+// Definisikan constant name
+const name = "Ninja ken";
+
+const introduce = (name) => {
+  // Print "Saya ____" diconsole
+  console.log(`Saya ${name}`);
+};
+
+// Panggil function introduce
+introduce("Guru Domba");
+
+// Print nilai dari constant name
+console.log(name);
